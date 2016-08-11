a30738df11f91d52546b9f480437e39919751ac3
Superman <superman08998@gmail.com>
2016-08-11 08:46:28 +0530

    feat(menu-typedef): add type definition for menu and menuItems

--- src/components/components.model.ts
+++ src/components/components.model.ts
@@ -1,1 +1,2 @@
 export * from './lean-scroll/lean-scroll';
+export * from './navigation-bar/navigation-bar';
---
+++ src/components/navigation-bar/navigation-bar.d.ts
@@ -0,0 +1,7 @@
+export type menuItem {
+    id: string,
+    title: string,
+    url: string,
+    ref: string
+}
+
