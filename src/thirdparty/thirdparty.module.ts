8854585ee5f1265cea3f49865d07d84658ce1016
Superman <superman08998@gmail.com>
2016-08-13 09:48:40 +0530

    feat(localization): integrate angular translate

--- src/thirdparty/thirdparty.module.ts
+++ src/thirdparty/thirdparty.module.ts
@@ -3,8 +3,9 @@ const moduleName: string = 'non.framework.thirdparty';
 declare const PerfectScrollbar: any;
 declare const MobileDetect: any;
 
-angular.module(moduleName, ['ui.router', 'ngAria', 'ngAnimate',
-    'ngMaterial', 'angular-loading-bar', 'ngCookies', 'ngMaterialSidemenu'])
+angular.module(moduleName,
+                [ 'ui.router', 'ngAria', 'ngAnimate', 'ngMaterial', 'angular-loading-bar', 'ngCookies', 'ngMaterialSidemenu',
+                    'ngMessages', 'ngResource', 'ngSanitize', 'pascalprecht.translate'])
     .constant('PerfectScrollbar', PerfectScrollbar)
     .constant('MobileDetect', MobileDetect);
 
