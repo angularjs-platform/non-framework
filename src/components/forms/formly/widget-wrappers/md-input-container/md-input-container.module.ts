5cebfbb14664f65cfc61cd8cbb1d0abf0b8c4adc
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2016-08-15 11:05:43 +0530

    feat(angular-fromly): integrate angular formly

---
+++ src/components/forms/formly/widget-wrappers/md-input-container/md-input-container.module.ts
@@ -0,0 +1,8 @@
+const moduleName: string = 'non.framework.components.forms.formly.widget-wrappers.md-input-container';
+
+import {FormlyConfig} from './md-input-container.config';
+
+angular.module(moduleName, [])
+    .config(FormlyConfig);
+
+export default moduleName;
---
+++ src/components/forms/formly/widget-wrappers/validation-messages/validation-messages.module.ts
@@ -0,0 +1,8 @@
+const moduleName: string = 'non.framework.components.forms.formly.widget-wrappers.validation-messages';
+
+import {FormlyConfig} from './validation-messages.config';
+
+angular.module(moduleName, [])
+    .config(FormlyConfig);
+
+export default moduleName;
