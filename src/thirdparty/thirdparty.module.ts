dbda9f7f15f749e785e72a6d66fd68bcf404499c
Superman <superman08998@gmail.com>
2016-09-28 14:19:14 +0530

    feat(animation): integrate animations library

--- src/thirdparty/thirdparty.module.ts
+++ src/thirdparty/thirdparty.module.ts
@@ -4,7 +4,7 @@ declare const PerfectScrollbar: any;
 declare const MobileDetect: any;
 
 angular.module(moduleName, [ 'ui.router', 'ngAria', 'ngAnimate', 'ngMaterial', 'angular-loading-bar', 'ngCookies', 'ngMaterialSidemenu',
-                    'ngMessages', 'ngResource', 'ngSanitize', 'pascalprecht.translate', 'ui.grid', 'formly'])
+                    'ngMessages', 'ngResource', 'ngSanitize', 'pascalprecht.translate', 'ui.grid', 'ng-fx', 'formly'])
     .constant('PerfectScrollbar', PerfectScrollbar)
     .constant('MobileDetect', MobileDetect);
 
