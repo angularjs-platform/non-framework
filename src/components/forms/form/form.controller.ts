42d990c41916455aca40e30d852e190c3fa96d84
Superman <superman08998@gmail.com>
2016-10-19 17:11:07 +0530

    feat(formly-button): formly button component

--- src/components/forms/form/form.controller.ts
+++ src/components/forms/form/form.controller.ts
@@ -3,12 +3,9 @@ import {FormConfiguration} from './form';
 export class FormController implements ng.IComponentController {
 
     public configuration: FormConfiguration;
-    public provider: Object;
     public submit: Function;
 
     constructor() {
-        this.configuration.options.data = {
-            provider: this.provider
-        };
+        // empty
     }
 }
