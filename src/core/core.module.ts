e2867b1d4f9049fb21c150ddb963976118738c7f
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2016-11-01 10:26:26 +0530

    feat(validation): implement validations framework

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -6,8 +6,8 @@ import authentication from './authentication/authentication.module';
 import layout from './layout/layout.module';
 import theme from './theme/theme.module';
 import menu from './menu/menu.module';
-import formSubmission from './form-submission/form-submission.module';
+import forms from './forms/forms.module';
 
-angular.module(moduleName, [http, authentication, config, layout, theme, menu, formSubmission]);
+angular.module(moduleName, [http, authentication, config, layout, theme, menu, forms]);
 
 export default moduleName;
