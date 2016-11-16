e8b7349d5800f1fef7ddf42619c098b86430d0c7
Superman <superman08998@gmail.com>
2016-11-16 15:03:15 +0530

    bugfix(typedef-correction): menulist is a object

--- src/core/menu/menu.d.ts
+++ src/core/menu/menu.d.ts
@@ -3,7 +3,7 @@ export interface IMenuService {
     getPrimaryMenuList(): {};
     getSecondaryMenuList(): {};
 
-    setPrimaryMenuList(primaryMenuList: {}[]): void;
-    setSecondaryMenuList(secondaryMenuList: {}[]): void;
+    setPrimaryMenuList(primaryMenuList: {}): void;
+    setSecondaryMenuList(secondaryMenuList: {}): void;
 }
 
